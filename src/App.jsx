a5885afbfdef1bd772a753f776cab1c2487f0cb4
Felipe Alves <felipe.alvescc16@gmail.com>
2025-11-02 12:58:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import Services from './components/Services'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import WhatsAppButton from './components/WhatsAppButton'
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Contact />
       </main>
       <Footer />
+      <WhatsAppButton />
     </div>
   )
 }
